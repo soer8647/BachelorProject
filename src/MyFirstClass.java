c372bcac455ba549d029a4b8cf0fd867423b97ab
soer8647 <Hjort.s@gmail.com>
2018-02-08 15:00:10 +0100

    init

---
+++ src/MyFirstClass.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Søren on 08-02-2018.
+ */
+public class MyFirstClass {
+    public MyFirstClass() {
+        System.out.println("lol");
+    }
+
+    public static void main(String[] args) {
+        MyFirstClass s = new MyFirstClass();
+    }
+
+}
