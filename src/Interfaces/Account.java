cae4b97e494d28d880684941cbb9be136ff253b3
Jacob <jacobcrawford1@hotmail.com>
2018-02-15 12:50:48 +0100

    Refactor public and privatekeys to be Types and not BigIntegers.

--- src/Interfaces/Account.java
+++ src/Interfaces/Account.java
@@ -1,5 +1,9 @@
 package Interfaces;
 
+import Crypto.Impl.PrivateKey;
+import Crypto.Impl.PublicKey;
+import Crypto.Interfaces.PublicKeyCryptoSystem;
+
 import java.math.BigInteger;
 
 public interface Account {
@@ -7,14 +11,14 @@ public interface Account {
     /**
      * @return  The address of this account
      */
-    BigInteger getAddress();
+    PublicKey getAddress();
 
     /**
      * @param transaction       The transaction that the account makes.
      * @return                  The signature on the transaction.
      */
     BigInteger sign(Transaction transaction);
+    PrivateKey getPrivateKey();
 
-
-    BigInteger getPrivateKey();
+    PublicKeyCryptoSystem getCryptoSystem();
 }
