06cf5c5f431fd5d68feb3c3e99ed7a63710cb0fc
Søren Hjort <hjort.s@gmail.com>
2018-02-27 15:16:09 +0100

    Refactored UDPclient to an actual class

--- Test/FakeClients/UDPConnectedBlockChains.java
+++ Test/FakeClients/UDPConnectedBlockChains.java
@@ -35,21 +35,7 @@ public class UDPConnectedBlockChains {
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
-        startUDP(9876,6789,IPAddress);
-        startUDP(6789,9876,IPAddress);
-    }
-
-    public static void startUDP(int myPort, int otherPort, InetAddress ip) {
-        BlockingQueue<Event> queue = new LinkedBlockingQueue<>();
-        Block genesisBlock =  new StandardBlock(new BigInteger("42"),20, new BigInteger("42"), 8, new ArrayListTransactions(),1,new CoinBaseTransactionStub());
-        TransactionManager transMan = new EmptyTransactionsManager();
-        PublicKeyCryptoSystem cs = Configuration.getCryptoSystem();
-        KeyPair node1KeyPair = cs.generateNewKeys(BigInteger.valueOf(3));
-        System.out.println(node1KeyPair.getPublicKey());
-        Address node1Address = new PublicKeyAddress(node1KeyPair.getPublicKey());
-        NodeRunner nodeRunner = new StandardNodeRunner(genesisBlock,queue,transMan,node1Address);
-        UDPReceiver receiver = new UDPReceiver(queue,myPort);
-        Publisher publisher = new UDPPublisher(ip,otherPort);
-        CommunicationHandler communicationHandler_B = new StandardCommunicationHandler(nodeRunner,publisher,queue);
+        UDPClient clientA = new UDPClient(9876,6789,IPAddress);
+        UDPClient clientB = new UDPClient(6789,9876,IPAddress);
     }
 }
