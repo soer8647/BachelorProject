ef8ba09a84f25e5de59ee489d29d173dcded8680
Shjorty <201505261@post.au.dk>
2018-05-30 19:28:02 +0200

    Added rsa using java security. Fixed  errors in experiment

---
+++ Test/Crypto/ExternalRSA.java
@@ -0,0 +1,43 @@
+package Crypto;
+
+import java.math.BigInteger;
+import java.security.*;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import javax.crypto.Cipher;
+
+public class ExternalRSA {
+    public static int getKeySize() {
+        return keySize;
+    }
+
+    public static void setKeySize(int keySize) {
+        ExternalRSA.keySize = keySize;
+    }
+
+    private static int keySize = 2048;
+
+    public static byte[] sign(BigInteger plainText, PrivateKey privateKey) throws Exception {
+        Signature privateSignature = Signature.getInstance("SHA256withRSA");
+        privateSignature.initSign(privateKey);
+        privateSignature.update(plainText.toByteArray());
+
+        byte[] signature = privateSignature.sign();
+
+        return signature;
+    }
+
+    public static boolean verify(BigInteger plainText, byte[] signature, PublicKey publicKey) throws Exception {
+        Signature publicSignature = Signature.getInstance("SHA256withRSA");
+        publicSignature.initVerify(publicKey);
+        publicSignature.update(plainText.toByteArray());
+
+        return publicSignature.verify(signature);
+    }
+
+    public static KeyPair buildKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(keySize, new SecureRandom());
+        return keyPairGenerator.genKeyPair();
+    }
+}
